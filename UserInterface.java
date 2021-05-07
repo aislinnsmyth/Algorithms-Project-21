193d5e317ca28adaff80c6be0a04bdf7bb7786f9
Aaryan Rai Gupta <79060113+aaryanraigupta@users.noreply.github.com>
2021-05-07 21:59:37 +0530

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -28,8 +28,7 @@ public class UserInterface {
                         DijkstraShortestPath.main();
        
                     case 2:
-             
-                        //implement stopfind class part two
+                        StopSearchFind.main();
                         break;
                     case 3:
                        ArrivalTime.main();
